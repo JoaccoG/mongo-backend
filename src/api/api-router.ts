38c9e22c223e5841e5bec55035a931c3d71bed79
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-01 18:44:37 +0100

    Add auth router

--- src/api/api-router.ts
+++ src/api/api-router.ts
@@ -1,8 +1,5 @@
 import express from 'express';
-import carsRouter from './cars/cars-router';
 
 const router = express.Router();
-
-router.use('/cars', carsRouter);
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import apiRouter from './api/api-router.js';
 import cors from 'cors';
+import apiRouter from './api/api-router.js';
+import authRouter from './api/auth/auth-router.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.disable('x-powered-by');
 
+app.use('/auth', authRouter);
 app.use('/api/v1', apiRouter);
 
 export default app;
