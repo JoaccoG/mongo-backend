1414fdd938a9df1f0182366badd836fecc59b135
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-02 18:08:18 +0100

    Add user endpoint to api router

--- src/api/api-router.ts
+++ src/api/api-router.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { usersRouter } from './users/users-router.js';
 
 const router = express.Router();
+
+router.use('/user', usersRouter);
 
 export default router;
