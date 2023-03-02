718f29f9d3ff81886562f5910cdc164cb7b725c0
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-02 09:33:45 +0100

    Add general error handler to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import apiRouter from './api/api-router.js';
 import authRouter from './api/auth/auth-router.js';
+import { errorHandler } from './utils/error-handler.js';
 
 const app = express();
 
@@ -15,5 +16,7 @@ app.disable('x-powered-by');
 
 app.use('/auth', authRouter);
 app.use('/api/v1', apiRouter);
+
+app.use(errorHandler);
 
 export default app;
