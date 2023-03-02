5d287a2bf34deea6d14e8acd9666ed62d7df3797
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-02 09:35:21 +0100

    Fix auth router, change jest and sonar coverage

--- src/api/auth/auth-router.ts
+++ src/api/auth/auth-router.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { validate } from 'express-validation';
 import { registerController } from './auth-controllers.js';
+import { authValidation } from './auth-validation.js';
 
 const authRouter = express.Router();
+
+authRouter.use(validate(authValidation));
 
 authRouter.route('/register').post(registerController);
 authRouter.route('/login').post();
